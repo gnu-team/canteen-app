c15bfbc101911b06172bd835ac6bcaf80c86f5b2
Austin Adams <git@austinjadams.com>
2016-09-16 18:29:30 -0400

    Use Dr. Water's MVC package structure
    To more easily see the separation between models, views, controllers,
    and the Main JavaFX application, split them into separate packages. Dr.
    Waters suggests this.

--- src/javafx/MainFXApplication.java
+++ src/javafx/MainFXApplication.java
@@ -1,4 +1,4 @@
-package sample;
+package javafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -6,11 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class MainFXApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
