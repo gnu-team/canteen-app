570c94b8638fbb34753ca2fc8a574e5e1928a076
Austin Adams <git@austinjadams.com>
2016-09-19 10:32:12 -0400

    model: Make User immutable

--- src/model/User.java
+++ src/model/User.java
@@ -11,20 +11,4 @@ public class User {
         this.user = user;
         this.password = password;
     }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
