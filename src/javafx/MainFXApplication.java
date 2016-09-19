6b1db73901971e54f4666aaa1a205adfabd47b8f
Austin Adams <git@austinjadams.com>
2016-09-19 09:20:45 -0400

    MainFXApp: Show welcome screen at startup
    The view is still missing, though, and some of the WelcomeController
    overlaps with Scene creation etc.

--- src/javafx/MainFXApplication.java
+++ src/javafx/MainFXApplication.java
@@ -5,19 +5,27 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class MainFXApplication extends Application {
     // TODO: Put this in a ResourceBundle instead
     private static final String TITLE = "Canteen: A Water Tracker";
 
+    private Stage stage;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
+    public void start(Stage primaryStage) throws IOException {
+        stage = primaryStage;
+
         primaryStage.setTitle(TITLE);
-        primaryStage.setScene(new Scene(root, 300, 275));
+        showWelcomeScreen();
         primaryStage.show();
     }
 
+    private void showWelcomeScreen() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/welcome.fxml"));
+        stage.setScene(new Scene(root));
+    }
 
     public static void main(String[] args) {
         launch(args);
