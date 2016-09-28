c98e2b5cc8d2b5250087d059e0ccd08d2aae5cac
Austin Adams <git@austinjadams.com>
2016-09-27 21:09:02 -0400

    WelcomeController: Populate account type ComboBox
    At initialization, add each account type from the model.AccountType enum
    to the account type ComboBox.

--- src/controller/WelcomeController.java
+++ src/controller/WelcomeController.java
@@ -6,7 +6,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import javafx.scene.control.ComboBox;
 import model.User;
+import model.AccountType;
 
 
 public class WelcomeController implements IMainAppReceiver {
@@ -17,9 +19,17 @@ public class WelcomeController implements IMainAppReceiver {
     private TextField PassWord;
 
     @FXML
+    private ComboBox<AccountType> accountTypeBox;
+
+    @FXML
     private Label Welcome;
     private User user = User.getDefaultUser();
     private MainFXApplication mainApplication;
+
+    @FXML
+    private void initialize() {
+        accountTypeBox.getItems().setAll(AccountType.values());
+    }
 
     public void setMainApp(MainFXApplication mainApplication) {
         this.mainApplication = mainApplication;
