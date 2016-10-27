dbf06c9de77c7b1574fd51989fa89a39ea7fd9da
Ph3nyclidine <p.claudepeon@gmail.com>
2016-10-26 20:57:17 -0400

    Historical report can now be viewed

--- src/controller/HistoricalReportController.java
+++ src/controller/HistoricalReportController.java
@@ -1,7 +1,27 @@
 package controller;
 
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.MainAppReceiver;
+import javafx.MainFXApplication;
+
 /**
  * Created by Ph3ncyclidine on 10/26/16.
  */
-public class HistoricalReportController {
+public class HistoricalReportController implements MainAppReceiver, MainControllerReceiver {
+    private MainFXApplication mainApp;
+    private MainController mainController;
+
+    @Override
+    public void setMainApp(MainFXApplication mainApp) {
+        this.mainApp = mainApp;
+    }
+
+    @Override
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
 }
