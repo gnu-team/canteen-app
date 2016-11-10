7eed2f6bc896fa1d680281fc498e139c2f8dc180
hyungsuk <grownhs@gmail.com>
2016-11-09 21:03:53 -0500

    test added

---
+++ src/model/addUserTest.java
@@ -0,0 +1,36 @@
+package model;
+
+import model.exception.DataException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Created by NikosD on 11/9/16.
+ */
+
+
+public class addUserTest {
+    MemoryDataSource mds;
+    boolean added = false;
+    boolean ifFail = true;
+    User a;
+    User b;
+    User c;
+    DataSuccessReceiver dsr = ()-> added = true;
+    DataErrorReceiver der = (DataException e)-> ifFail = false;
+    @Before
+    public void setUp() throws Exception {
+        mds = MemoryDataSource.getInstance();
+        a = new User("hyungsuk5057", "dhdish34846238", AccountType.USER);
+        b = new User("jadsld5857", "dklfjdopsfhaod234", AccountType.USER);
+    }
+
+    @Test
+    public void testAddFail() {
+        mds.addUser(b, dsr, der);
+        Assert.assertEquals("success to add", true, ifFail);
+        mds.addUser(b, dsr, der);
+        Assert.assertEquals("The account already exists", false, ifFail);
+    }
+}
